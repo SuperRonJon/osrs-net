18ba89cf94c00bfb1b4497e28019680581a1bc42
Patrick Seute <patrick.seute@radiantsolutions.com>
2019-04-04 10:30:15 -0400

    Add static id fetching functions

--- osrs-net/grandexchange/item.py
+++ osrs-net/grandexchange/item.py
@@ -1,4 +1,38 @@
+import json
+from util.file_io import read
+
+
 class Item:
-    def __init__(self, item_id=None, item_name=None):
+    def __init__(self, item_id, item_name, price_data, members):
         self.id = item_id
         self.name = item_name
+        self.price_data = price_data
+        self.members = members
+
+    @staticmethod
+    def _get_item_data():
+        text = read('osrs-net/resources/items.json')
+        return json.loads(text)
+
+    @staticmethod
+    def get_ids(name):
+        item_data = Item._get_item_data()
+        try:
+            return item_data[name.lower()]['id']
+        except KeyError:
+            matches = []
+            for item in item_data:
+                if name in item:
+                    matches.append(item_data[item]['id'])
+            return matches
+
+    @staticmethod
+    def id_to_name(id_num):
+        data = Item._get_item_data()
+        for item in data:
+            if data[item]['id'] == id_num:
+                return data[item]['name']
+
+        return None
+
+
